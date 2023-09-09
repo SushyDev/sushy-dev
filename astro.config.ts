9509470ac1cae3d3f88c1c6c128362178ef18c45
SushyDev <xsushy@hotmail.com>
2023-09-09 15:25:06 +0200

    rm polyfill test

--- astro.config.ts
+++ astro.config.ts
@@ -15,5 +15,10 @@ export default defineConfig({
         worker: {
             format: "es",
         },
+        build: {
+            modulePreload: {
+                polyfill: false,
+            }
+        }
     }
 });
