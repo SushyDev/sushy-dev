0e5fd10119411bf9fef4bbf04550f57ea18c146f
SushyDev <xsushy@hotmail.com>
2023-09-09 15:27:51 +0200

    mix-blend-difference

--- astro.config.ts
+++ astro.config.ts
@@ -14,11 +14,6 @@ export default defineConfig({
         },
         worker: {
             format: "es",
-        },
-        build: {
-            modulePreload: {
-                polyfill: false,
-            }
         }
     }
 });
