6bb12dbc480550c8baf79797a17caedce804a87b
SushyDev <xsushy@hotmail.com>
2023-09-11 20:33:56 +0200

    trigger on scroll

--- src/plugins/three-background/index.js
+++ src/plugins/three-background/index.js
@@ -1,2 +1,3 @@
 document.addEventListener('mousemove', () => import('./client.js'), { once: true });
 document.addEventListener('touchstart', () => import('./client.js'), { once: true });
+document.addEventListener('scroll', () => import('./client.js'), { once: true });
