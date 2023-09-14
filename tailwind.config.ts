4c76f9f4bc9ddb3719d14a46f93fea1584cb3e9c
SushyDev <xsushy@hotmail.com>
2023-09-14 17:04:21 +0200

    more tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,21 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                'accent': 'rgb(var(--accent))',
+                'accent-25': 'rgba(var(--accent), 0.25)',
+            },
+            backgroundImage: {
+                'accent-gradient': 'var(--accent-gradient)',
+            },
+            backgroundSize: {
+                '0%': '0%',
+                '400%': '400%',
+            }
+        }
+    }
 };
 
 export default config;
